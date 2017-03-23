0ab74e86c7e723f0af4ba6912576e8ed599501cd
Bonstor <glebvolohov@gmail.com>
2017-03-23 13:35:42 +0300

    update

--- optimize.m
+++ optimize.m
@@ -2,7 +2,7 @@ function [ proc ] = optimize( P,T )
 %OPTIMIZE Summary of this function goes here
 %   Detailed explanation goes here
 proc=[];
-for i=10:+10:150
+for i=150:+10:150
     net=feedforwardnet([i],'trainscg');
     net=train(net,P,T,'useParallel','yes');
     [p,in] = procent(sim(net,P),T);
